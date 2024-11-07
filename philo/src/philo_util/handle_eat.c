9cb0df2f698fc5da69720d5fbb4f1f681be85470
Lucas <denglucasyijin@gmail.com>
2024-11-08 01:42:56 +0800

    fix: solve last meal time exceeded

--- philo/src/philo_util/handle_eat.c
+++ philo/src/philo_util/handle_eat.c
@@ -6,7 +6,7 @@
 /*   By: geibo <geibo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 22:57:55 by geibo             #+#    #+#             */
-/*   Updated: 2024/11/08 01:14:27 by geibo            ###   ########.fr       */
+/*   Updated: 2024/11/08 01:40:16 by geibo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	handle_eat(t_philo *philo)
 	// if (!*philo->table->someone_died)
 	// {
 	pthread_mutex_lock(&philo->table->last_meal_mutex[philo->id - 1]);
-	philo->last_meal_time = get_time();
+	philo->last_meal_time = get_elapsed_time(philo->table->start_time);
 	pthread_mutex_unlock(&philo->table->last_meal_mutex[philo->id - 1]);
 	print_status(philo, "is eating");
 	philo->meals_eaten++;
